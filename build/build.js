451db2392ad84ff1283a93fe26abb45abd0d7c60
Faraway <zhiyuanhong@hustunique.com>
2017-08-04 23:53:13 +0800

    编译过程中将static目录合并

--- build/build.js
+++ build/build.js
@@ -3,6 +3,7 @@
  */
 
 const ora = require('ora')
+const fs = require('fs-extra')
 const path = require('path')
 const rm = require('rimraf')
 const chalk = require('chalk')
@@ -14,21 +15,22 @@ spinner.start()
 rm(path.resolve(__dirname, '../dist'), err => {
   if (err) throw err
   webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
     if (err) throw err
-    process.stdout.write(stats.toString(
-      {
+    fs.copy(path.resolve(__dirname, '../static'), path.resolve(__dirname, '../dist/static'), err => {
+      if (err) throw err
+      spinner.stop()
+      process.stdout.write(stats.toString({
         colors: true,
         modules: false,
         children: false,
         chunks: false,
         chunkModules: false
-      }
-    ) + '\n\n')
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
+      }) + '\n\n')
+      console.log(chalk.cyan('  Build complete.\n'))
+      console.log(chalk.yellow(
+        '  Tip: built files are meant to be served over an HTTP server.\n' +
+        '  Opening index.html over file:// won\'t work.\n'
+      ))
+    })
   })
 })
