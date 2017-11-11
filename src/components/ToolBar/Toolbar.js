89e8749a415974da67a6605faada4bb7b5b134cf
faraway <hzylovelyl@gmail.com>
2017-11-11 13:25:40 +0000

    feat: finish ToolBar(global bottom bar)

--- src/components/ToolBar/Toolbar.js
+++ src/components/ToolBar/Toolbar.js
@@ -2,15 +2,39 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import style from './TagSelector.styl'
+import style from './ToolBar.styl'
 
 class ToolBar extends React.Component {
   static mapStateToProps () {
+    return {}
+  }
+
+  state = {
+    activeTab: 0
   }
 
   render () {
+    console.log(this.props)
     return (
-      ''
+      <div className={style['wrapper']}>
+        <div className={style['wrapper']}>
+          { this.props.children }
+        </div>
+        <div className={style['tool-bar']}>
+          <div className={style['icon-wrapper']}>
+            <div className={style['icon-home']}></div>
+            <span className={style['title']}>首页</span>
+          </div>
+          <div className={style['icon-wrapper']}>
+            <div className={style['icon-kind']}></div>
+            <span className={style['title']}>分类</span>
+          </div>
+          <div className={style['icon-wrapper']}>
+            <div className={style['icon-mine']}></div>
+            <span className={style['title']}>我的</span>
+          </div>
+        </div>
+      </div>
     )
   }
 }
