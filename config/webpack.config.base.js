740f2142bd297d66405d5598c22471fdca812567
faraway <hzylovelyl@gmail.com>
2017-11-12 01:08:29 +0000

    fix: change base64 threshold

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -41,7 +41,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 5000,
+          limit: 2000,
           name: 'static/img/[name].[hash].[ext]'
         }
       },
@@ -49,7 +49,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 5000,
+          limit: 2000,
           name: 'static/fonts/[name].[hash].[ext]'
         }
       }
